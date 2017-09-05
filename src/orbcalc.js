78ecc27531e541afad50ff330daea1540f50a965
codythompson <cody.thompson22@gmail.com>
2017-09-04 17:33:05 -0700

    renamed orbcalc functions and added comment docs for future jsdoc

--- src/orbcalc.js
+++ src/orbcalc.js
@@ -1,15 +1,36 @@
-var orbitalVelocity = function (orbitalAltitude, planetRadius, accelAtSeaLevel) {
-  var result = planetRadius * Math.sqrt(accelAtSeaLevel/(planetRadius + orbitalAltitude));
+/**
+ * Calculates the orbital velocity from the given altitude of the major axis.
+ * @param {number} altitude - The altitude of the major axis of the orbit, in meters.
+ * @param {number} panetRadius - The radius of the planet, in meters.
+ * @param {number} accelAtSeaLevel - The acceleration due to gravity at sea level, in meters per seconds squared.
+ * @returns {number} The orbital velocity for the given major axis altitude, in meters per second.
+ */
+var velocityFromAltitude = function (altitude, planetRadius, accelAtSeaLevel) {
+  var result = planetRadius * Math.sqrt(accelAtSeaLevel/(planetRadius + altitude));
   return result;
 };
 
-var orbitalPeriod = function (orbitalAltitude, planetRadius, accelAtSeaLevel) {
-  var orbVel = orbitalVelocity(orbitalAltitude, planetRadius, accelAtSeaLevel);
-  var result = 2 * Math.PI * ((planetRadius + orbitalAltitude) / orbVel);
+/**
+ * Calculates the orbital period from the given altitude of the major axis.
+ * @param {number} altitude - The altitude of the major axis of the orbit, in meters.
+ * @param {number} panetRadius - The radius of the planet, in meters.
+ * @param {number} accelAtSeaLevel - The acceleration due to gravity at sea level.
+ * @returns {number} The orbital period for the given major axis altitude, in seconds.
+ */
+var periodFromAltitude = function (altitude, planetRadius, accelAtSeaLevel) {
+  var orbVel = velocityFromAltitude(altitude, planetRadius, accelAtSeaLevel);
+  var result = 2 * Math.PI * ((planetRadius + altitude) / orbVel);
   return result;
 };
 
-var orbitalAltitude = function (orbitalPeriod, planetRadius, accelAtSeaLevel) {
+/**
+ * Calculates the altitude of the major axis from the given altitude.
+ * @param {number} orbitalPeriod - The orbital period, in seconds.
+ * @param {number} panetRadius - The radius of the planet, in meters.
+ * @param {number} accelAtSeaLevel - The acceleration due to gravity at sea level.
+ * @returns {number} The major axis altitude, in meters.
+ */
+var altitudeFromPeriod = function (orbitalPeriod, planetRadius, accelAtSeaLevel) {
   // this formula was created by entering
   // solve t=2Pi((r+h)/(r(g/(r+h))^(1/2))) for h
   // into wolfram alhpa
@@ -20,6 +41,19 @@ var orbitalAltitude = function (orbitalPeriod, planetRadius, accelAtSeaLevel) {
   return h;
 };
 
-module.exports.orbitalVelocity = orbitalVelocity;
-module.exports.orbitalPeriod = orbitalPeriod;
-module.exports.orbitalAltitude = orbitalAltitude;
+/**
+ * Calculates the apogee from the given major axis altitude and desired perigee.
+ * @param {number} orbitalPeriod - The orbital period, in seconds.
+ * @param {number} panetRadius - The radius of the planet, in meters.
+ * @param {number} accelAtSeaLevel - The acceleration due to gravity at sea level.
+ * @returns {number} The major axis altitude, in meters.
+ */
+var apogeeFromMajorAxis = function (majorAxisAltitude, desiredPerigee) {
+  var apog = (2*majorAxisAltitude) - desiredPerigee;
+  return apog;
+};
+
+module.exports.velocityFromAltitude = velocityFromAltitude;
+module.exports.periodFromAltitude = periodFromAltitude;
+module.exports.altitudeFromPeriod = altitudeFromPeriod;
+module.exports.apogeeFromMajorAxis = apogeeFromMajorAxis;
