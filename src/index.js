b09eabdabe611385c9ca5eaadc8ddd49fbf55783
Cody Thompson <cody.thompson22@gmail.com>
2017-09-07 00:04:27 -0700

    added babel loader to webpack.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 var orbcalc = require ('./orbcalc');
 
+window.BabelTestClass = class {
+    constructor () {
+
+        this.check = 42;
+    }
+
+    get hmm () {
+        return this.check;
+    }
+    set hmm (newVal) {
+        this.check = newVal
+    }
+};
+
 window.orbcalc = orbcalc;
