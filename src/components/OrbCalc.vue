029308523a69bd60f887738dcae2a094f8f301fd
Cody Thompson <cody.thompson22@gmail.com>
2017-09-09 00:07:17 -0700

    I made a component

---
+++ src/components/OrbCalc.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="orb-calc-comp">
+        <planet-picker :celBodies="celBodies" :initialCelBody="celBodies.kerbin.name"></planet-picker>
+    </div>
+</template>
+
+<script>
+import PlanetPicker from './PlanetPicker'
+import celBodiesInfo from '../data/celBodiesInfo.json'
+
+export default {
+    name: 'OrbCalc',
+    components: {
+        PlanetPicker
+    },
+    data () {
+        return {
+            celBodies: celBodiesInfo
+        }
+    },
+    // computed: {
+    //     celBodies: function () {
+    //         return celBodiesInfo
+    //     }
+    // }
+}
+</script>
---
+++ src/components/PlanetPicker.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="planet-picker-comp">
+        <select v-model="selectedCelBody" @input="planetPicked">
+            <option v-for="(celBody, key) in celBodies" v-bind:key="key" :value="celBody.name">
+                {{celBody.name}}
+            </option>
+        </select>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PlanetPicker',
+    props: {
+        celBodies: {
+            type: Object,
+            required: true
+        },
+        initialCelBody: {
+            default: '',
+            type: String
+        }
+    },
+    data () {
+        return {
+            selectedCelBody: this.initialCelBody
+        }
+    },
+    methods: {
+        planetPicked: function () {
+            this.$emit('picked', this.selectedCelBody);
+        }
+    }
+}
+</script>
